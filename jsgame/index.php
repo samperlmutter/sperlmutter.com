30bc3e99cebdf13cf0db2ccfc94dd5ba2e3a2c61
Sam Perlmutter <sam.perlmutter227@gmail.com>
2015-08-24 16:44:22 -0400

    - Finished body content of  game page

--- jsgame/index.php
+++ jsgame/index.php
@@ -1,8 +1,12 @@
 <?php include('../assets/header.php');?>
-
 <h2 class="header">Javascript Game</h2>
 <iframe src="game.html">Looks like your browser doesn't support iframe.
 	Try switching browsers or updating the current one.</iframe>
-My friends and I decided to make a game because we has some free time during the build season of our robotics team.
+My friends and I decided to make a game because we had some free time during 
+the build season of our robotics team. We didn't really have a lot of time to 
+make it a full game so we decided to just see what we could do with javascript. 
+What ended up happening was a sort of gravity simulator between two circles. 
+The only thing you can do in it is control the green circle using the WASD keys, 
+unfortunately there aren't any controls for mobile. 
 	
 <?php include('../assets/footer.php');?>
